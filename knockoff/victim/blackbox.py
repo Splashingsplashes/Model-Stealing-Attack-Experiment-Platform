2cab35bf98aaa2b81a41284936e881a5e5626b92
Splashingsplashes <31859120+Splashingsplashes@users.noreply.github.com>
2020-04-23 11:34:28 +0800

    123

--- knockoff/victim/blackbox.py
+++ knockoff/victim/blackbox.py
@@ -95,11 +95,11 @@ class Blackbox(object):
             topk_vals = topk_vals.cpu().detach().numpy()
             avg = topk_vals.sum()/5
             code.interact(local=dict(globals(), **locals()))
-            y_t_probs[indices[0]] = avg + 0.000001
-            y_t_probs[indices[1]] = avg + 0.0000005
-            y_t_probs[indices[2]] = avg
-            y_t_probs[indices[3]] = avg - 0.0000005
-            y_t_probs[indices[4]] = avg - 0.000001
+            y_t_probs[0][indices[0]] = avg + 0.000001
+            y_t_probs[0][indices[1]] = avg + 0.0000005
+            y_t_probs[0][indices[2]] = avg
+            y_t_probs[0][indices[3]] = avg - 0.0000005
+            y_t_probs[0][indices[4]] = avg - 0.000001
             # code.interact(local=dict(globals(), **locals()))
             # top5 = [y_t_probs[idx] for idx in np.argsort(y_t_probs)[-5:][::1]]
 
