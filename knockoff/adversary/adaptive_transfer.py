62f4f3a0275948adc03ba8f416156514edcaf265
Splashingsplashes <31859120+Splashingsplashes@users.noreply.github.com>
2020-05-18 11:57:50 +0800

    123

--- knockoff/adversary/adaptive_transfer.py
+++ knockoff/adversary/adaptive_transfer.py
@@ -118,10 +118,10 @@ class AdaptiveAdversary(object):
                 y_hat = self.model(sampled_x)
 
                 sampled_x = sampled_x.cpu().numpy()[0]
-                # code.interact(local=dict(globals(), **locals()))
+                code.interact(local=dict(globals(), **locals()))
                 # sampled_x = np.transpose(sampled_x)
                 sampled_x = np.rollaxis(sampled_x, 0, 3)
-                code.interact(local=dict(globals(), **locals()))
+                # code.interact(local=dict(globals(), **locals()))
                 selected_x.append((sampled_x, y_output.cpu().squeeze().detach()))
 
                 pathCollection.append((path[0], y_output.detach().cpu().squeeze()))
