7010cf5646afa99ab09f95b0b48328f846da98f2
Splashingsplashes <31859120+Splashingsplashes@users.noreply.github.com>
2020-05-19 15:27:23 +0800

    123

--- knockoff/utils/model.py
+++ knockoff/utils/model.py
@@ -78,11 +78,9 @@ def train_step(model, train_loader, criterion, optimizer, epoch, device, log_int
                                 std=[0.229, 0.224, 0.225])
 
     for batch_idx, (inputs, targets) in enumerate(train_loader):
-        code.interact(local=dict(globals(), **locals()))
 
-        for i in batch_idx:
-            inputs[i] = trfm(inputs[i])
-            targets[i] = trfm(targets[i])
+        inputs[0] = trfm(inputs[0])
+        targets[0] = trfm(targets[0])
 
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
