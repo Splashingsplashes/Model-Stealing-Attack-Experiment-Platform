647307e05516b796753beb16dc948245eeb4182b
Splashingsplashes <31859120+Splashingsplashes@users.noreply.github.com>
2020-05-21 11:27:40 +0800

    123

--- knockoff/adversary/jacobian_transfer.py
+++ knockoff/adversary/jacobian_transfer.py
@@ -158,6 +158,10 @@ class JacobianAdversary(object):
                 aux_exp = np.exp(h_func)
                 probs = aux_exp / np.sum(aux_exp)
                 pbar.update()
+                if max(probs) > 0.9:
+                    code.interact(local=dict(globals(), **locals()))
+                print(np.partition(probs, -10)[-10:])
+                print(set(list(learning_rate)))
 
                 generated_sample = jacobian_input.detach().cpu()[0]
                 # generated_sample = np.rollaxis(generated_sample, 0, 3)
